7621a6da0d9215099984d6ec53c2b5a20a251fb2
Bagustyo <bagustyo@gmail.com>
2023-11-21 09:20:54 +0700

    Update five_principat_STP.py
    add  library

--- five_principat_STP.py
+++ five_principat_STP.py
@@ -1,20 +1,21 @@
+import random
+import itertools
+import operator
+import functools
+
 def get_var_name(variable):
-     for name, value in globals().items():
+    for name, value in globals().items():
         if value is variable:
             return name
 
-def test(XX,XY):
-    TT = list(itertools.combinations(XX,XY))
+def test(XX, XY):
+    TT = list(itertools.combinations(XX, XY))
     for x in TT:
-
         TY = [get_var_name(el) for el in x]
-
         APA = [set(i) for i in x]
         intersection = functools.reduce(operator.and_, APA)
-        if len(intersection) >= 1 :
+        if len(intersection) >= 1:
             print(f'{TY} : {intersection}')
-    
-
 
 ######################################### TEST ####################################
 
@@ -25,8 +26,9 @@ D = [random.randint(1, 10) for _ in range(10)]
 E = [random.randint(1, 10) for _ in range(5)]
 
 
+display(A,B,C,D,E)
+
 letters = [A, B, C, D, E]
 
-for x in range(len(letters)+1):
-    if int(x) > 1 :
-        test(letters,x)
+for x in range(2, len(letters) + 1):
+    test(letters, x)
